29e678372eb835dde38498ec85bed5bbd47ea449
Alwoch <salwoch@gmail.com>
2024-02-22 15:42:01 +0300

    update model

--- models.py
+++ models.py
@@ -7,11 +7,12 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    session_Id=Column(String(50))
+    username = Column(String(50),unique=True)
+    session_Id=Column(String(50),unique=True)
     last_updated = Column(
         TIMESTAMP,
         server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
         server_onupdate=FetchedValue()
     )
+    link_count_timestamp=Column(TIMESTAMP)
     link_count = Column(Integer,nullable=True)
