f6a9076a7709609a3bae1f10076859d2e3646522
Alwoch <salwoch@gmail.com>
2024-02-22 15:07:12 +0300

    update database configs

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ TOOL_TOOLSDB_PASSWORD=os.environ.get("TOOL_TOOLSDB_PASSWORD")
 SQLALCHEMY_DATABASE_URL = f"mariadb+pymysql://{TOOL_TOOLSDB_USER}:{TOOL_TOOLSDB_PASSWORD}@tools.db.svc.wikimedia.cloud/s55658__dlc?charset=utf8mb4"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
+    SQLALCHEMY_DATABASE_URL,echo=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
