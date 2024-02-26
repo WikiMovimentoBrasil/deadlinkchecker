62ef7bbcbdc4169c0304a9bb2c978eecd73056d3
Alwoch <salwoch@gmail.com>
2024-02-26 18:32:23 +0300

    get client

--- routes/auth.py
+++ routes/auth.py
@@ -98,5 +98,4 @@ async def oauth_callback(request: Request, db: Session = Depends(get_db)):
             
 
 # TODO make the redirect link dynamic depending an which wiki the user is on
-    # return RedirectResponse(url=f"https://en.wikipedia.org/wiki/Special:Deadlinkchecker/{session_id}")
-    return request.headers.get('host')
+    return RedirectResponse(url=f"https://en.wikipedia.org/wiki/Special:Deadlinkchecker/{session_id}")
